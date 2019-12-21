f2468ea86dda5f1140e5868a3d9267b06f2e8570
sa03576 <31242894+sa03576@users.noreply.github.com>
2019-12-21 16:10:11 +0500

    Login Page made

---
+++ WebApi/WebApi/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApi.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            //return "Hello World";
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            //return "testing, testing.....1, 2, 3";
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            
+            return View();
+        }
+
+    }
+}
